64468146c70e6dc37714dcba8235dbae01e852b0
unknown <sudebkarmokar8@gmail.com>
2021-12-09 19:26:48 +0600

    customize-section-crete

---
+++ src/component/Customize/Customize.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Customize.css'
+
+const Customize = () => {
+    return (
+        <div>
+            <div className="container-fluid customize-bg py-5">
+                <div className="container">
+                <div className="row d-flex justify-content-start align-items-center">
+                    <div className="col-md-4">
+                        <h3 className='fw-bold mb-5 mt-2'>Customized Stand <br />
+                        Construction
+                         </h3>
+                         <p className='w-75 mx-auto'>It's indeed a herculean task that goes in the making of customized stand construction. From wooden structure, electricity to flooring and graphics —everything needs to be in sync for the branding of the custom exhibition stands which goes with the exhibition colour style and product logo types. </p>
+                    
+                    </div>
+                    <div className="col-md-4">
+                        <h3 className='mb-5 fw-bold'>Country Pavilions</h3>
+                        <p className='w-75 mx-auto'>Country Pavilions on exhibited floor space convenes not just the evolving companies and their strength but opens doors for various businesses and decision makers to provide your company and country with opportunities. Indeed, it helps you speed up the Smart Utility transition. </p>
+                    </div>
+                    <div className="col-md-4">
+                        <h3 className='fw-bold mb-5'>Experiential <br /> Exhibition Stand</h3>
+                        <p className='w-75 mx-auto'>Any exhibition, large or small is one of the best ways to help meet, interact and win new customers face to face. Be it a bespoke stand-alone exhibition stand or dressing a shell scheme space - powerful brand messaging ensuring lasting take-away memories for visitors. </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Customize;
